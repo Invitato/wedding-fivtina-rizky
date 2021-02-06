1be78df15c9dfe0acf2d2107623ccc8c6f768835
Indra Kusuma <indrakusuma.udinus@gmail.com>
2021-02-06 23:39:29 +0700

    feat: add animation QRcode

--- src/components/GiftCard/index.js
+++ src/components/GiftCard/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import WithAnimation from '../WithAnimation';
 import OVO from './assets/ovo.jpeg';
 import LinkAja from './assets/link-aja.jpeg';
 
@@ -8,19 +10,23 @@ function GiftCard() {
   return (
     <div css={styWrapper}>
       <div className="row">
-        <div className="text-wish text-center">
-          <h2 className="main-font pr-co text__title" style={{ fontSize: '4rem' }}>
-            Tanda Kasih
-          </h2>
-          <p>
-            Bagi Bpk/Ibu/Sdr/i yang ingin memberikan tanda kasih kepada mempelai dan keluarga <br /> dapat melakukan
-            QR Code uang elektronik berikut:
-          </p>
-        </div>
-        <div className="img">
-          <img src={OVO} className="img__ovo" />
-          <img src={LinkAja} className="img__linkaja" />
-        </div>
+        <WithAnimation>
+          <div className="text-wish text-center">
+            <h2 className="main-font pr-co text__title" style={{ fontSize: '4rem' }}>
+              Tanda Kasih
+            </h2>
+            <p>
+              Bagi Bpk/Ibu/Sdr/i yang ingin memberikan tanda kasih kepada mempelai dan keluarga <br /> dapat melakukan
+              QR Code uang elektronik berikut:
+            </p>
+          </div>
+        </WithAnimation>
+        <WithAnimation delay={100}>
+          <div className="img">
+            <img src={OVO} className="img__ovo" />
+            <img src={LinkAja} className="img__linkaja" />
+          </div>
+        </WithAnimation>
       </div>
     </div>
   );
