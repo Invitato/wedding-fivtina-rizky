f22e41f7aaa93bd6125532ec7559f5889ef1172b
Indra Kusuma <indrakusuma.udinus@gmail.com>
2021-02-06 19:40:15 +0700

    feat: add photo

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -1,4 +1,23 @@
-import G1 from './assets/1.jpg';
+import G1 from './assets/1.jpeg';
+import G2 from './assets/2.jpeg';
+import G3 from './assets/3.jpeg';
+import G4 from './assets/4.jpeg';
+import G5 from './assets/5.jpeg';
+import G6 from './assets/6.jpeg';
+import G7 from './assets/7.jpeg';
+import G8 from './assets/8.jpeg';
+import G9 from './assets/9.jpeg';
+import G10 from './assets/10.jpeg';
+import G11 from './assets/11.jpeg';
+import G12 from './assets/12.jpeg';
+import G13 from './assets/13.jpeg';
+import G15 from './assets/15.jpeg';
+import G16 from './assets/16.jpeg';
+import G17 from './assets/17.jpeg';
+import G21 from './assets/21.jpeg';
+import G24 from './assets/24.jpeg';
+import G26 from './assets/26.jpeg';
+import G28 from './assets/28.jpeg';
 
 export const photos = [
   {
@@ -6,7 +25,83 @@ export const photos = [
     thumbnail: G1,
   },
   {
-    original: G1,
-    thumbnail: G1,
+    original: G3,
+    thumbnail: G3,
+  },
+  {
+    original: G4,
+    thumbnail: G4,
+  },
+  {
+    original: G5,
+    thumbnail: G5,
+  },
+  {
+    original: G6,
+    thumbnail: G6,
+  },
+  {
+    original: G7,
+    thumbnail: G7,
+  },
+  {
+    original: G10,
+    thumbnail: G10,
+  },
+  {
+    original: G11,
+    thumbnail: G11,
+  },
+  {
+    original: G16,
+    thumbnail: G16,
+  },
+  {
+    original: G17,
+    thumbnail: G17,
+  },
+  {
+    original: G21,
+    thumbnail: G21,
+  },
+  {
+    original: G24,
+    thumbnail: G24,
+  },
+  {
+    original: G26,
+    thumbnail: G26,
+  },
+  {
+    original: G28,
+    thumbnail: G28,
+  },
+
+  /**
+   * potrait
+   */
+  {
+    original: G15,
+    thumbnail: G15,
+  },
+  {
+    original: G2,
+    thumbnail: G2,
+  },
+  {
+    original: G8,
+    thumbnail: G8,
+  },
+  {
+    original: G9,
+    thumbnail: G9,
+  },
+  {
+    original: G12,
+    thumbnail: G12,
+  },
+  {
+    original: G13,
+    thumbnail: G13,
   },
 ];
